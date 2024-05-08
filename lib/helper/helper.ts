0af86a9e7c984fdd986ffc7b9e514de003b9e0db
Aman <aman@Amans-MacBook-Air.local>
2024-05-08 17:16:16 +0530

    dynamic ratings and reviews

--- lib/helper/helper.ts
+++ lib/helper/helper.ts
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export function debounce(func, delay) {
   let timeoutId;
 
@@ -10,4 +12,73 @@ export function debounce(func, delay) {
       func.apply(context, args);
     }, delay);
   };
+}
+
+export async function appendReviewAndRating(products: any) {
+  try {
+    const reviews = await getReviews();
+    const ratings = await getRatings();
+
+    reviews.forEach((review: any) => {
+      const product = products.find((product: any) => {
+        const id = getProductId(product.id);
+        return id === review.external_product_id;
+      });
+      if (product) {
+        product.reviews = review;
+      }
+    });
+
+    ratings.forEach((rating: any) => {
+      const product = products.find((product: any) => {
+        const id = getProductId(product.id);
+        return id === rating.external_product_id;
+      });
+      if (product) {
+        product.ratings = rating;
+      }
+    });
+
+    return products;
+  } catch (error) {
+    console.log('error', error);
+    throw error;
+  }
+}
+
+const getProductId = (id: string): string => {
+  const parts = id.split('/');
+
+  // Extracting the part from the last occurrence of slash till the first occurrence
+  const extractedId = parts.slice(-1)[0];
+
+  return extractedId as string;
+};
+
+async function getReviews() {
+  const reviewApiOptions = {
+    method: 'GET',
+    url: 'https://api.fera.ai/v3/private/reviews',
+    headers: {
+      accept: 'application/json',
+      'SECRET-KEY': process.env.FERA_FOXTALE_SECRET_KEY
+    }
+  };
+  const response = await axios.request(reviewApiOptions);
+  const reviews = response.data.data;
+  return reviews;
+}
+
+async function getRatings() {
+  const ratingApiOptions = {
+    method: 'GET',
+    url: 'https://api.fera.ai/v3/private/ratings',
+    headers: {
+      accept: 'application/json',
+      'SECRET-KEY': process.env.FERA_FOXTALE_SECRET_KEY
+    }
+  };
+  const response = await axios.request(ratingApiOptions);
+  const ratings = response.data.data;
+  return ratings;
 }
