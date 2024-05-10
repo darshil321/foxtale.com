037dee04300b4d2c7629cc8908522f0f977ea703
darshil321 <148060388+darshil321@users.noreply.github.com>
2024-05-10 14:23:06 +0530

    added mailchimp newsletter (#18)
    Co-authored-by: parth2024 <156824891+parth2024@users.noreply.github.com>
    Co-authored-by: Darshil <Darshil>

---
+++ app/api/subscribe/route.ts
@@ -0,0 +1,29 @@
+// pages/api/subscribe.ts
+import axios from 'axios';
+import { NextResponse } from 'next/server';
+
+export async function POST(req: Request) {
+  const url = `https://us18.api.mailchimp.com/3.0/lists/${process.env.NEXT_PUBLIC_MAILCHIMP_LIST_KEY}/members`;
+  const headers = {
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_MAILCHIMP_API_KEY}`
+  };
+  const reqPayload = await req.json();
+  const data = {
+    email_address: reqPayload.email,
+    status: 'subscribed'
+  };
+  try {
+    const response = await axios.post(url, data, { headers });
+    if (response.status === 200)
+      return NextResponse.json({ message: 'Subscribed!!' }, { status: 200 });
+    return NextResponse.json({ message: 'Not Subscribed!!' }, { status: 500 });
+  } catch (error) {
+    console.error('Mailchimp API error:', error);
+    return NextResponse.json(
+      { message: 'Not Subscribed!!' },
+      {
+        status: 400
+      }
+    );
+  }
+}
--- components/elements/custom-input-with-btn.tsx
+++ components/elements/custom-input-with-btn.tsx
@@ -1,5 +1,7 @@
+'use client';
+import axios from 'axios';
 import clsx from 'clsx';
-import React from 'react';
+import React, { useRef } from 'react';
 
 const CustomInputBtn = ({
   className,
@@ -10,42 +12,65 @@ const CustomInputBtn = ({
   className?: string;
   buttonText?: string;
   text?: string;
-  onClick?: () => void;
   type?: 'email' | 'password' | 'text';
 }) => {
-  return (
-    <div className="flex flex-row gap-2 ">
-      <div className="relative mt-2 flex w-full items-center">
-        <span className="absolute">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="dark mx-3 h-6 w-6 "
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
-            />
-          </svg>
-        </span>
+  const emailRef = useRef<HTMLInputElement>(null);
 
-        <input
-          type={type}
-          placeholder={text}
-          className={clsx(
-            'block w-full rounded-[4px] border  border-gray-400 bg-white py-[16px] pl-11 pr-5  placeholder-black  focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 rtl:pl-5 rtl:pr-11',
-            className
-          )}
-        />
-        <button className="absolute right-1.5 rounded-[4px]  bg-black px-5 py-3 text-[12px] tracking-widest text-white">
-          {buttonText}
-        </button>
+  const handleNewsletterSubscription = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!emailRef.current) {
+      alert('Please enter a valid email address');
+      return;
+    }
+    try {
+      const response = await axios.post('/api/subscribe', {
+        email: emailRef.current.value
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.log('error', error);
+    }
+  };
+  return (
+    <form onSubmit={handleNewsletterSubscription}>
+      <div className="flex flex-row gap-2 ">
+        <div className="relative mt-2 flex w-full items-center">
+          <span className="absolute">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="dark mx-3 h-6 w-6 "
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+              />
+            </svg>
+          </span>
+
+          <input
+            type={type}
+            placeholder={text}
+            className={clsx(
+              'block w-full rounded-[4px] border  border-gray-400 bg-white py-[16px] pl-11 pr-5  placeholder-black  focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 rtl:pl-5 rtl:pr-11',
+              className
+            )}
+            ref={emailRef}
+          />
+          <button
+            type="submit"
+            onClick={handleNewsletterSubscription}
+            className="absolute right-1.5 rounded-[4px]  bg-black px-5 py-3 text-[12px] tracking-widest text-white"
+          >
+            {buttonText}
+          </button>
+        </div>
       </div>
-    </div>
+    </form>
   );
 };
 
