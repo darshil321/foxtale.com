e49c3d6328cebd9c81367a0616466e82d36e8583
Darshil <Darshil>
2024-05-12 23:43:43 +0530

    done

--- components/product/variant-selector.tsx
+++ components/product/variant-selector.tsx
@@ -6,12 +6,6 @@ import { createUrl } from 'lib/utils';
 import Link from 'next/link';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
-// type Combination = {
-//   id: string;
-//   availableForSale: boolean;
-//   [key: string]: string | boolean;
-// };
-
 export function VariantSelector({
   options,
   variants
@@ -35,64 +29,79 @@ export function VariantSelector({
     );
   };
 
-  return options.map((option) => (
-    <dl className="mb-6" key={option.id}>
-      <dd className="flex w-full divide-x divide-black border border-black ">
-        <p>{option.name}</p>
-        {option.values.map((value) => {
-          const optionNameLowerCase = option.name.toLowerCase();
+  return (
+    <div className="relative">
+      {' '}
+      {/* Relative container for absolute positioning */}
+      {options.map((option) => {
+        const isColorOption = option.name.toLowerCase() === 'color';
+        // const isLastOption = index === options.length - 1;
 
-          // Base option params on current params so we can preserve any other param state in the url.
-          const optionSearchParams = new URLSearchParams(searchParams.toString());
-
-          // Update the option params using the current option to reflect how the url *would* change,
-          // if the option was clicked.
-          optionSearchParams.set(optionNameLowerCase, value);
-          const optionUrl = createUrl(pathname, optionSearchParams);
-
-          // Find the corresponding variant for the selected options
-          const selectedVariant = findVariantForOptionValue(value);
-
-          // Determine if the variant is available for sale
-          const isAvailableForSale = selectedVariant?.availableForSale;
-
-          // The option is active if it's in the url params.
-          const isActive = searchParams.get(optionNameLowerCase) === value;
-
-          return (
-            <button
-              key={value}
-              aria-disabled={!isAvailableForSale}
-              disabled={!isAvailableForSale}
-              onClick={() => {
-                router.replace(optionUrl, { scroll: false });
-              }}
-              title={`${option.name} ${value}${isAvailableForSale ? ' (Out of Stock)' : ''}`}
-              className={clsx(
-                'flex   flex-1 flex-col items-center justify-center gap-0 bg-neutral-100 py-3 text-sm uppercase tracking-wider text-gray-700 md:text-base ',
-                {
-                  'cursor-default bg-gray-600 bg-opacity-30 font-medium text-black': isActive,
-                  'ring-1 ring-transparent transition duration-200 ease-in-out hover:text-black':
-                    !isActive && isAvailableForSale,
-                  'relative z-10 cursor-not-allowed overflow-hidden bg-neutral-100 text-black ring-1 ring-neutral-300 before:absolute before:inset-x-0  before:h-px before:-rotate-45 before:bg-neutral-300 before:transition-transform ':
-                    !isAvailableForSale
-                }
-              )}
+        return (
+          <dl className="mb-6" key={option.id}>
+            <dd
+              className={clsx('flex w-full ', {
+                ' divide-x divide-black border border-black': !isColorOption
+              })}
             >
-              {value}
-              <span>₹{selectedVariant?.price.amount}</span>
-            </button>
-          );
-        })}
-        <Link
-          href={'#combos'}
-          className={clsx(
-            'flex flex-1 items-center justify-center  scroll-smooth  bg-neutral-100 py-4 text-sm uppercase tracking-wider text-stone-700 hover:text-black md:text-base '
-          )}
-        >
-          Combos
-        </Link>
-      </dd>
-    </dl>
-  ));
+              {option.values.map((value) => {
+                const optionNameLowerCase = option.name.toLowerCase();
+                const optionSearchParams = new URLSearchParams(searchParams.toString());
+                optionSearchParams.set(optionNameLowerCase, value);
+                const optionUrl = createUrl(pathname, optionSearchParams);
+                const selectedVariant = findVariantForOptionValue(value);
+                const isAvailableForSale = selectedVariant?.availableForSale;
+                const isActive = searchParams.get(optionNameLowerCase) === value;
+
+                return (
+                  <div
+                    key={value}
+                    aria-disabled={!isAvailableForSale}
+                    disabled={!isAvailableForSale}
+                    onClick={() => {
+                      if (isAvailableForSale) {
+                        router.replace(optionUrl, { scroll: false });
+                      }
+                    }}
+                    title={`${option.name} ${value}${isAvailableForSale ? ' (Out of Stock)' : ''}`}
+                    className={clsx(
+                      'flex flex-1 cursor-pointer flex-col items-center justify-center gap-0 py-3 text-sm uppercase tracking-wider text-gray-700 md:text-base',
+                      {
+                        'cursor-default bg-gray-600 bg-opacity-30 font-medium text-black': isActive,
+                        'ring-1 ring-transparent transition duration-200 ease-in-out hover:text-black':
+                          !isActive && isAvailableForSale,
+                        'relative z-10 cursor-not-allowed overflow-hidden text-black ring-1 ring-neutral-300 before:absolute before:inset-x-0 before:h-px before:-rotate-45 before:bg-neutral-300 before:transition-transform':
+                          !isAvailableForSale,
+                        'mr-2 h-full max-h-8 w-full max-w-[1.5rem] rounded-full bg-green-300':
+                          isColorOption,
+                        [`bg-${value.toLowerCase()}`]: isColorOption && isAvailableForSale,
+                        'bg-gray-300': isColorOption && !isAvailableForSale
+                      }
+                    )}
+                  >
+                    {!isColorOption && value}
+                    {!isColorOption && <span>₹{selectedVariant?.price.amount}</span>}
+                  </div>
+                );
+              })}
+              {!isColorOption && (
+                <Link
+                  href={'#combos'}
+                  className={clsx(
+                    'relative flex flex-1 items-center justify-center scroll-smooth bg-neutral-100 py-4 text-sm uppercase tracking-wider text-stone-700 hover:text-black md:text-base '
+                  )}
+                >
+                  Combos
+                </Link>
+              )}
+            </dd>
+          </dl>
+        );
+      })}
+      {/* Text strip */}
+      <div className="absolute bottom-0 left-0 w-full bg-green-500 py-1 text-center text-white">
+        Save Extra
+      </div>
+    </div>
+  );
 }
