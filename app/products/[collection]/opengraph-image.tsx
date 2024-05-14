d4dd78c75de286461ad20534c846714e49541bef
Darshil <Darshil>
2024-05-14 12:18:53 +0530

    done

--- app/products/[collection]/opengraph-image.tsx
+++ app/products/[collection]/opengraph-image.tsx
@@ -1,8 +1,6 @@
 import OpengraphImage from 'components/opengraph-image';
 import { getCollection } from 'lib/shopify';
 
-export const runtime = 'edge';
-
 export default async function Image() {
   const collection = await getCollection('all');
   const title = collection?.seo?.title || collection?.title;
