212dfc01b2cac0a4722a941593b9f1ccd9c3191a
Darshil <Darshil>
2024-05-14 18:43:58 +0530

    done

--- components/product/hero-banner-slider.tsx
+++ components/product/hero-banner-slider.tsx
@@ -1,16 +1,30 @@
-// @ts-ignore
-import { EmblaOptionsType } from 'embla-carousel';
-import EmblaHeroCarousel from './embla-hero-carousel';
+// // @ts-ignore
+// import { EmblaOptionsType } from 'embla-carousel';
+// import EmblaHeroCarousel from './embla-hero-carousel';
 import '../../assets/styles/hero-slider.css';
-
-const OPTIONS: EmblaOptionsType = { dragFree: false, loop: true };
-const SLIDE_COUNT = 5;
-const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
+import Image from 'next/image';
+// import { EmblaCarouselType, EmblaEventType, EmblaOptionsType } from 'embla-carousel';
+// const OPTIONS: EmblaOptionsType = { dragFree: false, loop: true };
+// const SLIDE_COUNT = 5;
+// const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
 
 const HeroBannerSlider = () => {
   return (
     <div>
-      <EmblaHeroCarousel slides={SLIDES} options={OPTIONS} />
+      {/* <EmblaHeroCarousel slides={SLIDES} options={OPTIONS} /> */}{' '}
+      <div className="hero_embla__parallax__layer ">
+        <Image
+          priority
+          height={475}
+          width={470}
+          objectFit="cover"
+          className="hero_embla__slide__img embla__parallax__img"
+          src={`/Images/banner.png`}
+          sizes="(max-width: 768px) 70vw"
+          alt="Your alt text"
+          quality={80}
+        />
+      </div>
     </div>
   );
 };
