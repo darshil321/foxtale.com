50eef5201e4750fe1fad1cac53637cdc231e4a53
Darshil <Darshil>
2024-05-16 14:06:13 +0530

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,10 +6,9 @@ import WrapperContainer from 'components/layout/wrapper-container';
 import Provider from '../store/store-provider';
 import Banner from 'components/layout/navbar/banner';
 import { Poppins } from 'next/font/google';
-// import InitialData from '@/components/initial-data';
-// import { getMetaObjects } from '@/lib/shopify';
+import { getMetaObjects } from '@/lib/shopify';
 // import dynamic from 'next/dynamic';
-// const InitialData = dynamic(() => import('@/components/initial-data'), { ssr: false });
+// const InitialData = dynamic(() => import('@/components/initial-data'));
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -45,6 +44,20 @@ export const metadata = {
 };
 
 export default async function RootLayout({ children }: { children: ReactNode }) {
+  const promises = [
+    getMetaObjects('gifts'),
+    getMetaObjects('freebies'),
+    getMetaObjects('magic_link')
+  ];
+
+  const results = await Promise.allSettled(promises);
+
+  // const giftsCoupon = results[0]?.status === 'fulfilled' ? results[0].value : null;
+  // const freebieCoupons = results[1]?.status === 'fulfilled' ? results[1].value : null;
+  // const magicLinks = results[2]?.status === 'fulfilled' ? results[2].value : null;
+
+  console.log('results', results);
+
   return (
     <html lang="en">
       <link rel="preconnect" href={process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN} />{' '}
@@ -55,7 +68,13 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
           <WrapperContainer>
             <Navbar />
           </WrapperContainer>
-          <Suspense fallback={null}>{/* <InitialData /> */}</Suspense>
+          <Suspense fallback={null}>
+            {/* <InitialData
+              giftsCoupon={giftsCoupon}
+              freebieCoupons={freebieCoupons}
+              magicLinks={magicLinks}
+            /> */}
+          </Suspense>
           <main className={poppins.className}>{children}</main>
         </Provider>
       </body>
