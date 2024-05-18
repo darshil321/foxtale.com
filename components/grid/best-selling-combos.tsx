0beb2ab32f0f382abba66e3019d3bc7e1b151b70
nayanDevX <nayan@devxconsultancy.coom>
2024-05-18 11:23:31 +0530

    mainnnn

---
+++ components/grid/best-selling-combos.tsx
@@ -0,0 +1,46 @@
+import { GridTileImage } from 'components/grid/tile';
+import { getProduct } from 'lib/shopify';
+import type { Product } from 'lib/shopify/types';
+import Link from 'next/link';
+
+async function BestSellingCombosItem({ priority, handle }: { priority?: boolean; handle: string }) {
+  const item = await getProduct(handle);
+  if (!item) return;
+  return (
+    <div className="">
+      <Link className="relative block h-full w-full" href={`/product/${item.handle}`}>
+        <GridTileImage
+          product={item}
+          src={item.featuredImage.url}
+          priority={priority}
+          alt={item.title}
+          label={{
+            title: item.title as string,
+            amount: item.priceRange.maxVariantPrice.amount,
+            currencyCode: item.priceRange.maxVariantPrice.currencyCode
+          }}
+        />
+      </Link>
+    </div>
+  );
+}
+
+export async function BestSellingCombos({ product }: { product: Product }) {
+  const filteredDataByKey = product?.metafields?.find(
+    (item: any) => item?.key === 'pair_best_with'
+  );
+  const products = filteredDataByKey?.value?.split('@');
+  console.log('filteredDataByKey', products);
+  return (
+    <section className="grid grid-cols-2 gap-4 pb-4">
+      <h2 className="col-span-2 gap-3 text-center text-2xl font-medium leading-7">
+        Best Selling Combos
+      </h2>
+      {products?.map((item: string, index: number) => (
+        <BestSellingCombosItem key={index} item={product} handle={item} />
+      ))}
+      {/* <BestSellingCombosItem size="full" item={homepageItems[1]} />
+      <BestSellingCombosItem size="full" item={homepageItems[2]} /> */}
+    </section>
+  );
+}
