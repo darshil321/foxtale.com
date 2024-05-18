205f974a4b93957f16e3e4a8b4e0a4ca8b1d1973
Darshil <Darshil>
2024-05-19 01:43:34 +0530

    done

--- utils/image-utils.ts
+++ utils/image-utils.ts
@@ -1,22 +1,22 @@
-import sharp from 'sharp';
+// import sharp from 'sharp';
 
-export async function compressAndConvertToBase64(url: string, compressionPercentage: number) {
-  try {
-    // Fetch the image from the URL
-    const response = await fetch(url);
-    const arrayBuffer = await response.arrayBuffer();
+// export async function compressAndConvertToBase64(url: string, compressionPercentage: number) {
+//   try {
+//     // Fetch the image from the URL
+//     const response = await fetch(url);
+//     const arrayBuffer = await response.arrayBuffer();
 
-    // Compress the image using Sharp
-    const compressedImage = await sharp(arrayBuffer)
-      .resize({ quality: compressionPercentage })
-      .toBuffer();
+//     // Compress the image using Sharp
+//     const compressedImage = await sharp(arrayBuffer)
+//       .resize({ quality: compressionPercentage })
+//       .toBuffer();
 
-    // Convert the compressed image to Base64
-    const base64Image = Buffer.from(compressedImage).toString('base64');
+//     // Convert the compressed image to Base64
+//     const base64Image = Buffer.from(compressedImage).toString('base64');
 
-    return `data:image/jpeg;base64,${base64Image}`;
-  } catch (error) {
-    console.error('Error compressing and converting image:', error);
-    return null;
-  }
-}
+//     return `data:image/jpeg;base64,${base64Image}`;
+//   } catch (error) {
+//     console.error('Error compressing and converting image:', error);
+//     return null;
+//   }
+// }
