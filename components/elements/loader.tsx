3a8fbc5c6d40213cd1e7eb27c1106825993def93
Darshil <Darshil>
2024-05-21 12:59:45 +0530

    done

---
+++ components/elements/loader.tsx
@@ -0,0 +1,15 @@
+'use client';
+import React from 'react';
+import Navbar from '../layout/navbar';
+import Banner from '../layout/navbar/banner';
+
+const Loader = () => {
+  return (
+    <div>
+      <Banner />
+      <Navbar />
+    </div>
+  );
+};
+
+export default Loader;
--- store/store-provider.tsx
+++ store/store-provider.tsx
@@ -1,12 +1,25 @@
 'use client';
 import React from 'react';
 import { Provider } from 'react-redux';
-
-import { store } from './store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './store';
+import Loader from '@/components/elements/loader';
 
 export const Providers: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  //  hello world
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate
+        persistor={persistor}
+        loading={
+          <>
+            <Loader />
+          </>
+        }
+      >
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 };
 
 export default Providers;
