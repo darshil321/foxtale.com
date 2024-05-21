e2902af43af5c9cd3e7c650a0ca60cef6ad2dd31
Darshil <Darshil>
2024-05-21 18:21:12 +0530

    done

--- app/product/[handle]/page.tsx
+++ app/product/[handle]/page.tsx
@@ -74,6 +74,8 @@ export default async function ProductPage({
 }) {
   const product = await getProduct(params.handle);
 
+  console.log('product', product);
+
   if (!product) return notFound();
 
   const productJsonLd = {
