b7b7b1a346a6e703efce06cf7b3668e5e15ba2e7
chiragdevx <chirag@devxconsultancy.com>
2024-05-21 11:02:18 +0530

    fix

--- store/store-provider.tsx
+++ store/store-provider.tsx
@@ -3,13 +3,12 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from './store';
-import LoadingDots from '@/components/loading-dots';
+import Navbar from '@/components/layout/navbar';
 
 export const Providers: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  //  hello world
   return (
     <Provider store={store}>
-      <PersistGate persistor={persistor} loading={<LoadingDots />}>
+      <PersistGate persistor={persistor} loading={<Navbar></Navbar>}>
         {children}
       </PersistGate>
     </Provider>
