34540f0aba1a1f5bdfecc1f2df6802e567b9be64
nayanDevX <nayan@devxconsultancy.coom>
2024-05-23 16:29:32 +0530

    priority

--- store/store-provider.tsx
+++ store/store-provider.tsx
@@ -1,14 +1,16 @@
 'use client';
 import React from 'react';
 import { Provider } from 'react-redux';
-import { store } from './store';
+import { store, persistor } from './store';
+import { PersistGate } from 'redux-persist/integration/react';
+import Loader from '@/components/elements/loader';
 
 export const Providers: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <Provider store={store}>
-      {/* <PersistGate persistor={persistor} loading={<Loader />}> */}
-      {children}
-      {/* </PersistGate> */}
+      <PersistGate persistor={persistor} loading={<Loader />}>
+        {children}
+      </PersistGate>
     </Provider>
   );
 };
