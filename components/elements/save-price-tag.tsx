cfcc5aa41fb209e1b8a9d5b345ed41b34284af91
Darshil <Darshil>
2024-05-28 20:57:20 +0530

    done

--- components/elements/save-price-tag.tsx
+++ components/elements/save-price-tag.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const SavePriceTag = ({ savePrice }: { savePrice: number | string }) => {
   return (
-    <div className="absolute left-4 top-5 rounded-full bg-white p-2 text-center  text-xs  uppercase text-[#F05001]">
+    <div className="absolute left-2 top-2 rounded-full bg-white  p-2 text-center text-xs uppercase  text-[#F05001]  md:left-4 md:top-5">
       <p>SAVE</p>
       <p className="font-bold">₹ {savePrice}</p>
     </div>
