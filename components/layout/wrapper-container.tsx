e6805aa780df27c38f971bfbdee461a4747e8e25
nayanDevX <nayan@devxconsultancy.coom>
2024-05-30 19:29:08 +0530

    banner fixed

--- components/layout/wrapper-container.tsx
+++ components/layout/wrapper-container.tsx
@@ -6,7 +6,7 @@ interface WrapperProps {
 
 const WrapperContainer: React.FC<WrapperProps> = ({ children }) => {
   return (
-    <div className=" lg:mx-30 max-w-[1440px] md:mx-10 md:px-4 lg:px-5 xl:mx-[121px]">
+    <div className=" lg:mx-30 max-w-[1440px] md:mx-10 md:max-w-[1920px] md:px-4 lg:max-w-[2400px] lg:px-5 xl:mx-[121px]">
       {children}
     </div>
   );
