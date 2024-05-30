38ebbf5fbf1a989a36b5f067084df914ab89b040
chiragdevx <chirag@devxconsultancy.com>
2024-05-30 17:25:31 +0530

    fix

--- components/layout/navbar/banner.tsx
+++ components/layout/navbar/banner.tsx
@@ -3,12 +3,6 @@ import React from 'react';
 import { trackEvent } from 'utils/mixpanel';
 
 const Banner = () => {
-  const dynamicValue = 'FOX499';
-  const staticPart = '[ Code: ';
-  const closingBracket = ' ]';
-
-  const result = staticPart + dynamicValue + closingBracket;
-
   const handleBannerClick = (e: React.MouseEvent<HTMLDivElement>) => {
     e.preventDefault();
     trackEvent('Announcement Bar Clicked', {});
@@ -18,12 +12,7 @@ const Banner = () => {
     <div onClick={handleBannerClick} className="bg-black bg-gradient-to-r text-white ">
       <div className="container mx-auto flex items-center justify-center py-1">
         <div className="text-white">
-          <span className="text-sm font-semibold text-white">
-            Buy singles @flat ₹399 or ₹499&nbsp;
-          </span>
-          <a href="#" rel="noopener noreferrer" className="text-sm">
-            {result}
-          </a>
+          <span className="text-sm  text-white">Exclusive prices for our new users!</span>
         </div>
       </div>
     </div>
