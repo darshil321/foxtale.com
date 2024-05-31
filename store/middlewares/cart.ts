8713cb8262a30b2f0e28bcb3daf5e6123ac75ba1
chiragdevx <chirag@devxconsultancy.com>
2024-05-31 13:39:33 +0530

    fix

--- store/middlewares/cart.ts
+++ store/middlewares/cart.ts
@@ -129,7 +129,7 @@ export function* manageCartSaga(action: {
       const res = yield call({ fn: updateCart, context: null }, cartId, updatePayload);
       console.log('willUpdate', updatePayload, res);
     }
-    yield delay(1000);
+    yield delay(700);
     yield put(setLoading(false));
   } catch (error) {
     console.log('error', error);
