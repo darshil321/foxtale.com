b38481804725150ed5aca4f744cb1e6bf6e731da
chiragdevx <chirag@devxconsultancy.com>
2024-06-04 11:07:04 +0530

    fix

--- components/cart/add-to-cart-button.tsx
+++ components/cart/add-to-cart-button.tsx
@@ -6,13 +6,10 @@ import { useAppDispatch } from 'store/hooks';
 import { cartActions } from 'store/actions/cart.action';
 import { getDefaultVariant } from '@/lib/helper/helper';
 import { setCartOpen } from '@/store/slices/cart-slice';
-import { trackEvent } from 'utils/mixpanel';
 import { toast, Slide } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useDispatch } from 'react-redux';
-import { sendGAEvent } from '@next/third-parties/google';
 
-import { fbEvent } from 'utils/facebook-pixel';
 import { scrollToElementById } from '@/lib/utils';
 const ToastContent: React.FC = () => {
   const dispatch = useDispatch();
@@ -83,63 +80,104 @@ function SubmitButton({
             })
           );
 
+          const parts = product.id.split('/');
+          const id = parts[parts.length - 1];
+
           if (window && window.dataLayer) {
-            window.dataLayer.push({ ecommerce: null, attribute: null, user: null });
             window.dataLayer.push({
               event: 'add_to_cart',
-              attribute: 'pixel',
-              ecommerce: {
-                currency: 'INR'
+
+              ga: {
+                Product_Name: product.title,
+                Product_Url: '',
+                Product_Price: product?.priceRange?.maxVariantPrice?.amount,
+                Price_Currency: product?.priceRange?.maxVariantPrice?.currencyCode,
+                Source: '',
+                Category: '',
+                Tags: product.tags,
+                Variant_SKU: ''
+              },
+              fb: {
+                content_ids: [id],
+                content_name: product.title,
+                content_type: 'product',
+                content_category: 'recommended',
+                contents: [
+                  {
+                    id: id,
+                    quantity: 1,
+                    price: product?.priceRange?.minVariantPrice?.amount,
+                    title: product.title,
+                    handle: product.handle,
+                    description: product.description
+                  }
+                ],
+                // content_collections: product.collections,
+                currency: product?.priceRange?.minVariantPrice?.currencyCode,
+                value: product?.priceRange?.minVariantPrice?.amount,
+                num_items: 1
+                //===
+                // fbc: getFbpCookie()
+              },
+              mixpanel: {
+                Product_Name: product.title,
+                Product_Url: '',
+                Product_Price: product?.priceRange?.maxVariantPrice?.amount,
+                Price_Currency: product?.priceRange?.maxVariantPrice?.currencyCode,
+                Source: '',
+                Category: '',
+                Tags: product.tags,
+                Variant_SKU: ''
               }
             });
           }
-          sendGAEvent('event', 'add_to_cart', {
-            currency: 'INR',
-            value: product?.priceRange?.maxVariantPrice?.amount,
-            items: [
-              {
-                item_id: selectedVariantId,
-                item_name: product?.title,
-                price: product?.priceRange?.maxVariantPrice?.amount,
-                quantity: 1
-              }
-            ]
-          });
+          // sendGAEvent('event', 'add_to_cart', {
+          //   currency: 'INR',
+          //   value: product?.priceRange?.maxVariantPrice?.amount,
+          //   items: [
+          //     {
+          //       item_id: selectedVariantId,
+          //       item_name: product?.title,
+          //       price: product?.priceRange?.maxVariantPrice?.amount,
+          //       quantity: 1
+          //     }
+          //   ]
+          // });
 
-          trackEvent('Add To Cart', {
-            Product_Name: product.title,
-            Product_Url: '',
-            Product_Price: product?.priceRange?.maxVariantPrice?.amount,
-            Price_Currency: product?.priceRange?.maxVariantPrice?.currencyCode,
-            Source: '',
-            Category: '',
-            Tags: product.tags,
-            Variant_SKU: ''
-          });
-          const parts = product.id.split('/');
-          const id = parts[parts.length - 1];
-          fbEvent('AddToCart', {
-            content_ids: [id],
-            content_name: product.title,
-            content_type: 'product',
-            content_category: 'recommended',
-            contents: [
-              {
-                id: id,
-                quantity: 1,
-                price: product?.priceRange?.minVariantPrice?.amount,
-                title: product.title,
-                handle: product.handle,
-                description: product.description
-              }
-            ],
-            // content_collections: product.collections,
-            currency: product?.priceRange?.minVariantPrice?.currencyCode,
-            value: product?.priceRange?.minVariantPrice?.amount,
-            num_items: 1
-            //===
-            // fbc: getFbpCookie()
-          });
+          // trackEvent('Add To Cart', {
+          //   Product_Name: product.title,
+          //   Product_Url: '',
+          //   Product_Price: product?.priceRange?.maxVariantPrice?.amount,
+          //   Price_Currency: product?.priceRange?.maxVariantPrice?.currencyCode,
+          //   Source: '',
+          //   Category: '',
+          //   Tags: product.tags,
+          //   Variant_SKU: ''
+          // });
+          // const parts = product.id.split('/');
+          // const id = parts[parts.length - 1];
+          // fbEvent('AddToCart', {
+          //   content_ids: [id],
+          //   content_name: product.title,
+          //   content_type: 'product',
+          //   content_category: 'recommended',
+          //   contents: [
+          //     {
+          //       id: id,
+          //       quantity: 1,
+          //       price: product?.priceRange?.minVariantPrice?.amount,
+          //       title: product.title,
+          //       handle: product.handle,
+          //       description: product.description
+          //     }
+          //   ],
+          //   // content_collections: product.collections,
+          //   currency: product?.priceRange?.minVariantPrice?.currencyCode,
+          //   value: product?.priceRange?.minVariantPrice?.amount,
+          //   num_items: 1
+          //   //===
+          //   // fbc: getFbpCookie()
+          // });
         }}
         aria-label="Add to cart"
         className={clsx(buttonClasses, {
