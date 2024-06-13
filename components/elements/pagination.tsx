8834c0ee880cc3340992d0b5dfc3a11265bdad0c
Darshil <Darshil>
2024-06-13 13:22:13 +0530

    done

--- components/elements/pagination.tsx
+++ components/elements/pagination.tsx
@@ -11,7 +11,7 @@ interface PaginationProps {
 const Pagination: React.FC<PaginationProps> = ({ currentPage, totalPages, onPageChange }) => {
   if (!totalPages || totalPages <= 0) return null;
 
-  const maxPagesToShow = 10;
+  const maxPagesToShow = 4;
   const startPage = Math.floor((currentPage - 1) / maxPagesToShow) * maxPagesToShow + 1;
   const endPage = Math.min(startPage + maxPagesToShow - 1, totalPages);
 
@@ -23,9 +23,9 @@ const Pagination: React.FC<PaginationProps> = ({ currentPage, totalPages, onPage
 
   return (
     <div className="flex h-full w-full items-center justify-between py-3 sm:px-2">
-      <div className="flex w-full flex-col  items-center justify-center space-y-2 sm:flex-1 sm:justify-between md:flex-row">
+      <div className="flex w-full flex-col items-center justify-center space-y-2 sm:flex-1 sm:justify-between md:flex-row">
         <div>
-          <p className=" px-2 text-sm text-gray-700">
+          <p className="px-2 text-sm text-gray-700">
             Showing <span className="font-medium">{(currentPage - 1) * 10 + 1}</span> to{' '}
             <span className="font-medium">{Math.min(currentPage * 10, totalPages * 10)}</span> of{' '}
             <span className="font-medium">{totalPages * 10}</span> results
@@ -34,9 +34,30 @@ const Pagination: React.FC<PaginationProps> = ({ currentPage, totalPages, onPage
         <div>
           <nav className="isolate inline-flex -space-x-px rounded-md" aria-label="Pagination">
             <button
+              onClick={() => handlePageChange(1)}
+              disabled={currentPage === 1}
+              className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 disabled:opacity-50"
+            >
+              <span className="sr-only">First</span>
+              <svg
+                aria-hidden="true"
+                focusable="false"
+                height="17"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 384 512"
+                data-fa-i2svg=""
+              >
+                <path
+                  fill="currentColor"
+                  d="M20.2 247.5L167 99.5c4.7-4.7 12.3-4.7 17 0l19.8 19.8c4.7 4.7 4.7 12.3 0 17L85.3 256l118.5 119.7c4.7 4.7 4.7 12.3 0 17L184 412.5c-4.7 4.7-12.3 4.7-17 0l-146.8-148c-4.7-4.7-4.7-12.3 0-17zm160 17l146.8 148c4.7 4.7 12.3 4.7 17 0l19.8-19.8c4.7-4.7 4.7-12.3 0-17L245.3 256l118.5-119.7c4.7-4.7 4.7-12.3 0-17L344 99.5c-4.7-4.7-12.3-4.7-17 0l-146.8 148c-4.7 4.7-4.7 12.3 0 17z"
+                ></path>
+              </svg>
+            </button>
+            <button
               onClick={() => handlePageChange(currentPage - 1)}
               disabled={currentPage === 1}
-              className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400  hover:bg-gray-50 focus:z-20 focus:outline-offset-0 disabled:opacity-50"
+              className="relative inline-flex items-center px-2 py-2 text-gray-400 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 disabled:opacity-50"
             >
               <span className="sr-only">Previous</span>
               <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
@@ -47,10 +68,10 @@ const Pagination: React.FC<PaginationProps> = ({ currentPage, totalPages, onPage
                   key={page}
                   onClick={() => handlePageChange(page)}
                   aria-current={currentPage === page ? 'page' : undefined}
-                  className={`relative z-10 inline-flex items-center px-2 py-2 text-sm  sm:px-4 sm:py-2 ${
+                  className={`relative z-10 inline-flex items-center px-2 py-2 text-sm sm:px-4 sm:py-2 ${
                     currentPage === page
-                      ? '  font-semibold text-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2'
-                      : 'font-medium text-gray-400 ring-gray-300 hover:bg-gray-50 focus:outline-offset-0 '
+                      ? 'font-semibold text-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2'
+                      : 'font-medium text-gray-400 ring-gray-300 hover:bg-gray-50 focus:outline-offset-0'
                   }`}
                 >
                   {page}
@@ -60,10 +81,31 @@ const Pagination: React.FC<PaginationProps> = ({ currentPage, totalPages, onPage
             <button
               onClick={() => handlePageChange(currentPage + 1)}
               disabled={currentPage === totalPages}
-              className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400  hover:bg-gray-50 focus:z-20 focus:outline-offset-0 disabled:opacity-50"
+              className="relative inline-flex items-center px-2 py-2 text-gray-400 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 disabled:opacity-50"
             >
               <span className="sr-only">Next</span>
               <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+            </button>
+            <button
+              onClick={() => handlePageChange(totalPages)}
+              disabled={currentPage === totalPages}
+              className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 disabled:opacity-50"
+            >
+              <span className="sr-only">Last</span>
+              <svg
+                aria-hidden="true"
+                focusable="false"
+                height="17"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 384 512"
+                data-fa-i2svg=""
+              >
+                <path
+                  fill="currentColor"
+                  d="M363.8 264.5L217 412.5c-4.7 4.7-12.3 4.7-17 0l-19.8-19.8c-4.7-4.7-4.7-12.3 0-17L298.7 256 180.2 136.3c-4.7-4.7-4.7-12.3 0-17L200 99.5c4.7-4.7 12.3-4.7 17 0l146.8 148c4.7 4.7 4.7 12.3 0 17zm-160-17L57 99.5c-4.7-4.7-12.3-4.7-17 0l-19.8 19.8c-4.7 4.7-4.7 12.3 0 17L138.7 256 20.2 375.7c-4.7 4.7-4.7 12.3 0 17L40 412.5c4.7 4.7 12.3 4.7 17 0l146.8-148c4.7-4.7 4.7-12.3 0-17z"
+                ></path>
+              </svg>
             </button>
           </nav>
         </div>
